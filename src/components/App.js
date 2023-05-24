c1d4d38495b96eea1263cbcac046402ef51f1c85
Jose <joseluis0528@gmail.com>
2023-05-23 21:30:00 -0500

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,12 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
-function App() {
+function App(props) {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home {...user}/>
+      <About {...user}/>
     </div>
   );
 }
